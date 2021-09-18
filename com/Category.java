6386f0cb5ab74b665d37394944534db002dcc987
unknown <mahelidutta12@gmail.com>
2021-09-18 22:51:48 +0530

    flipkart

---
+++ com/Category.java
@@ -0,0 +1,15 @@
+package com;
+
+public class Category {
+    private String name;
+
+    public Category(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+}
---
+++ com/OrderStatus.java
@@ -0,0 +1,5 @@
+package com;
+
+public enum OrderStatus {
+    CREATED,PENDING, UNSHIPPED, SHIPPED, CANCELLED, COMPLETED;
+}
